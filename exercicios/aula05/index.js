6c5eceef2b2eef71fc6465354fd7356f7c8da425
lim2-cmyk <lim2@aluno.ifnmg.edu.br>
2026-04-09 17:03:06 +0000

    feat: alter codigo

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,25 +7,57 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  // escreva seu código aqui
+    if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else if (idade <= 59) {
+    return "adulto";
+  } else {
+    return "idoso";
+  }
 }
-
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+  const desconto = preco * (percentual / 100);
+  return preco - desconto;
 }
+
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+   if (nota >= 60) {
+    return "aprovado";
+  } else if (nota >= 40) {
+    return "recuperação";
+  } else {
+    return "reprovado";
+  }
+
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+  let soma = 0;
+
+  for (let numero of numeros) {
+    if (numero % 2 === 0) {
+      soma += numero;
+    }
+  }
+
+  return soma;
 }
+
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contador = 0;
+
+  for (let caractere of texto) {
+    if (caractere === letra) {
+      contador++;
+    }
+  }
+  return contador;
 }
